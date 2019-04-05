83a555229cbad9639af44d642fc81ae6c4179547
dtatkinson <46680338+dtatkinson@users.noreply.github.com>
2019-04-05 13:54:33 +0100

    App Complete
    success on all 3 tests

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,6 +1,20 @@
 function getSequence(n) {
   // TODO: Implement your code to replace the line below
-  return [1, 1, 2, 3, 5, 8, 13, 21, 34, 55].slice(0, n);
+	var first = 0;
+	var second = 1;
+	var result = second;
+	var numbers =[];
+  for(i=0;i<20;i++)
+  {
+	  
+	  numbers.push(result);
+	 result = first+second;
+	 first = second;
+	 second = result;
+	  
+  }
+  return numbers.slice(0,n); 
+  //return [1, 1, 2, 3, 5, 8, 13, 21, 34, 55].slice(0, n);
 }
 
 module.exports = {
